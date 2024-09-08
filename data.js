c389216559f02e0481243077ae36677338a9e5f8
artinramizi11 <artin11@outlook.com>
2024-09-08 23:02:51 +0200

    Garden Website

---
+++ data.js
@@ -0,0 +1,47 @@
+let clients = [ {
+    name1: 'Maria Gang',
+    description1: 'We’re impressed with your garden business’s offerings and expertise. Your innovative designs and high-quality plants perfectly align with our vision. We look forward to collaborating on creating stunning outdoor spaces.',
+image1: '/images/profile1.jpg',
+
+name2: 'Jonathan Blue',
+    description2: 'Were thrilled with your garden business creative approach and premium plant selections. Your commitment to exceptional design and sustainability matches our vision perfectly. Excited to start transforming our outdoor spaces with you',
+    image2: '/images/profile2.jpg'
+},
+{
+    name1: 'Hovey Airley',
+    description1:'Your expert designs and beautiful plant choices have transformed our outdoor space into a stunning retreat.',
+    image1: '/images/profile3.jpg',
+
+    name2: 'Harvis Marty',
+    description2: 'beautiful plant choices have transformed our outdoor space into a stunning retreat.',
+    image2: '/images/profile4.jpg'
+}, {
+    
+        name1: 'Luis Juheny',
+        description1: 'beautiful plant choices have transformed our outdoor space into a stunning retreat.',
+        image1: '/images/profile5.jpg',
+
+        name2: 'Tristy Hailey',
+        description2: 'beautiful plant choices have transformed our outdoor space into a stunning retreat.',
+        image2: '/images/profile6.jpg'
+    }
+
+];
+
+let section1data = [
+    {
+dataTitle: 'Crafting Natural Beauty',
+dataDescription: 'Eleveate your sorroundings and experience the allure of nature in your own backyard'
+    },
+    {
+        dataTitle: 'Outdoor Living Design',
+        dataDescription:'Transform your surroundings and embrace the charm of nature right in your own backyard.'
+
+    },
+    {
+        dataTitle: 'Home Landscaping',
+        dataDescription: 'Enrich your space and immerse yourself in the enchanting allure of nature from the comfort of your backyard.'
+
+
+    }
+]
---
+++ script.js
@@ -0,0 +1,134 @@
+
+const section1InfoButtons = document.querySelector('.section1InfoButtons');
+const section1Title = document.querySelector('.section1Title');
+const section1Description = document.querySelector('.section1Description');
+
+const section5Clients = document.querySelector('.section5clients')
+const section5LeftButton = document.querySelector('.section5LeftButton');
+const section5RightButton = document.querySelector('.section5RightButton');
+const section5ClientDescription = document.querySelectorAll('.clientDescription')
+const section5ClientImage = document.querySelectorAll('.clientImage')
+const section5circle = document.querySelectorAll('.circle');
+const section5circles = document.querySelector('.section5circles')
+
+
+section1InfoButtons.addEventListener('click', function(e){
+    if(e.target.classList.contains(`button${e.target.dataset.id}`)){
+        section1Title.textContent = `${section1data[e.target.dataset.id].dataTitle}`;
+        section1Description.textContent = `${section1data[e.target.dataset.id].dataDescription}`
+    } 
+    for(let i=0; i<section1InfoButtons.children.length; i++){
+        section1InfoButtons.children[i].style.backgroundColor = 'rgb(117, 182, 117)';
+        section1InfoButtons.children[i].addEventListener('mouseover', function(e){
+            e.target.style.backgroundColor = 'green'
+        })
+        section1InfoButtons.children[i].addEventListener('mouseout', function(e){
+            e.target.style.backgroundColor = 'rgb(117, 182, 117)'
+        })
+    
+        if(e.target == section1InfoButtons.children[i]){
+            section1InfoButtons.children[i].style.backgroundColor = 'green';
+            e.target.addEventListener('mouseout', function(e){
+                e.target.style.backgroundColor = 'green'
+            })
+
+        
+        } else if(e.target == section1InfoButtons) {
+            return;
+        }
+    }
+})
+
+
+
+let sliderPage = 0;
+let maxSliderPage = clients.length - 1;
+
+
+
+section5RightButton.addEventListener('click',function(){
+if(sliderPage == maxSliderPage) {
+        sliderPage = 0;
+    } else {
+        sliderPage++;
+    }
+
+    updateTestimonailsClients()
+    
+    });
+
+
+    section5LeftButton.addEventListener('click',function(){
+       
+        if(sliderPage == 0){
+            sliderPage = 3;
+        } else {
+            sliderPage--;
+        }
+
+        updateTestimonailsClients()
+    })
+
+function updateTestimonailsClients(){
+    section5Clients.innerHTML = `
+    <div class="section5client">
+            <div class="section5clientImage">
+                <img src="${clients[sliderPage].image1}" alt="" class="clientImage">
+
+            </div>
+            <div class="section5clientDescription">
+                <span class="material-symbols-outlined">
+                    chat
+                    </span>
+<p class="clientDescription">${clients[sliderPage].description1}</p>
+<h1 class="clientName">${clients[sliderPage].name1}</h1>
+            </div>
+        </div>
+        <div class="section5client">
+            <div class="section5clientImage">
+                <img src="${clients[sliderPage].image2}" alt="" class="clientImage">
+
+            </div>
+            <div class="section5clientDescription">
+                <span class="material-symbols-outlined">
+                    chat
+                </span>
+<p class="clientDescription">${clients[sliderPage].description2}</p>
+<h1 class="clientName">${clients[sliderPage].name2}</h1>
+            </div>
+        </div>
+   `;
+   for(let i=0; i<section5circle.length; i++){
+    section5circle[i].style.backgroundColor = 'gray'
+   }
+   section5circle[sliderPage].style.backgroundColor = 'green'
+}
+
+updateTestimonailsClients();
+
+
+section5circles.addEventListener('click',function(e){
+    sliderPage = e.target.dataset.id;
+    updateTestimonailsClients()
+})
+
+let openMenu = true;
+
+document.querySelector('.openMenu').addEventListener('click',function(){
+    if(openMenu) {
+        document.querySelector('.nav-lists').style.display = 'flex';
+    document.querySelector('.openMenu').innerHTML = `
+    <span class="material-symbols-outlined">
+close
+</span>`; 
+openMenu = false;
+    }  else {
+        document.querySelector('.nav-lists').style.display = 'none';
+        document.querySelector('.openMenu').innerHTML = `
+         <span class="material-symbols-outlined">
+                    menu
+                    </span>`;
+                    openMenu = true;
+    }
+
+})
